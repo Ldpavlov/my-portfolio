2541622c8eb5d788bdd4dd0531f1d54e9fb53e80
Ldpavlov <87299254+Ldpavlov@users.noreply.github.com>
2023-11-23 10:16:35 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function () { myFunction() };
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get the "To the top" link element
+//     var scrollToTopBtn = document.querySelector(".w3-top");
+
+//     // Add a click event listener to the link
+//     scrollToTopBtn.addEventListener("click", function() {
+//         // Scroll smoothly to the top of the page
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//     });
+// });
+
